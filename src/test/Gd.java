19b6118e2e9eb91284fe799297e110286dda5ad1
Zzanggaring <limja77770@naver.com>
2023-06-17 16:53:12 +0900

    주석

--- src/test/Gd.java
+++ src/test/Gd.java
@@ -3,9 +3,9 @@ package test;
 public class Gd {
 
 	public static void main(String[] args) {
-		System.out.println("하이");
-		System.out.println("하이2");
-		System.out.println("하이second Branch 하이를 그냥 3개 다 추가");
+//		System.out.println("하이");
+//		System.out.println("하이2");
+//		System.out.println("하이second Branch 하이를 그냥 3개 다 추가");
 		System.out.println("pull해봐");
 
 	}
