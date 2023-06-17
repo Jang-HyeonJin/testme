38bd3b054664b0d22c5ef0e5e825d6120db25791
Zzanggaring <limja77770@naver.com>
2023-06-17 13:43:07 +0900

    1343

---
+++ chap02/src/chap02/Ex07.java
@@ -0,0 +1,27 @@
+package chap02;
+
+public class Ex07 {
+
+	public static void main(String[] args) {
+//		byte result=10+20;
+//		System.out.println(result);
+//		byte x=10;
+//		byte y=20;
+//		
+//		//byte result1=x+y;
+//		int result2=x+y;
+//		double result5=1.2f+3.4;
+//		System.out.println(result5);
+//		double res=10.2f+3.4;
+//		System.out.println(res);
+//		int intValue=10;
+//		double doubleValue=5.5;
+//		int result=intValue+(int)doubleValue;
+//		System.out.println(result);
+		int x=1;
+		int y =2;
+		double result=(double)x/y;
+		System.out.println(result);
+	}
+
+}
