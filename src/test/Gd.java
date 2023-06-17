4d279aa41c32e6faec25a5a0fc1ad2aaba1e3168
Zzanggaring <limja77770@naver.com>
2023-06-17 15:21:12 +0900

    3번째 가본다

--- src/test/Gd.java
+++ src/test/Gd.java
@@ -3,7 +3,7 @@ package test;
 public class Gd {
 
 	public static void main(String[] args) {
-		System.out.println("하이Third Branch 하이 리셋 후 추가");
+		System.out.println("하이Third Branch 하이 리셋 후 3번째추가");
 	}
 
 }
